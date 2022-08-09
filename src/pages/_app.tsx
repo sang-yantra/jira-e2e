d7f897685ea8677dc6a0b10e7a1b4669f1eb34da
Anup Mahato <anupmahato033@gmail.com>
2022-08-09 11:51:05 +0530

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { AppRouter } from './api/trpc/[trpc]';
+import superjson from "superjson"
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : 'http://localhost:3000/api/trpc';
+
+    return {
+      url,
+      transformer: superjson
+      /**
+       * @link https://react-query-v3.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
+})(MyApp);
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+import { prisma } from "../db/client";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = (props: any) => {
+  const { data, isLoading } = trpc.useQuery(["questions"]);
+
+  if (isLoading || !data) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        POLLS hi there
+        {data?.map((poll) => {
+          return (
+            <div key={poll.id}>
+              <span>POLLs</span>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+};
+
+// export const getServerSideProps = async () => {
+
+//   const questions = await prisma.pollQuestion.findMany();
+//   return {
+//     props : {
+//       questions: JSON.stringify(questions)
+//     }
+//   }
+// }
+
+export default Home;
